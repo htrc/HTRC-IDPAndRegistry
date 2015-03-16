7a684953047db1e206b3ece813285e0f12bedb1a
Milinda Pathirage <milinda.pathirage@gmail.com>
2015-03-16 13:15:44 -0400

    patches for wso2 is original jars to fix some issues related to SAML and OAuth2 userinfo endpoint

---
+++ patches/org.wso2.carbon.identity.oauth.endpoint/4.2.3/src/main/java/org/wso2/carbon/identity/oauth/endpoint/user/impl/UserInfoJWTResponse.java
@@ -0,0 +1,55 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.wso2.carbon.identity.oauth.endpoint.user.impl;
+
+import org.apache.amber.oauth2.common.exception.OAuthSystemException;
+import org.apache.oltu.oauth2.jwt.JWTBuilder;
+import org.apache.oltu.oauth2.jwt.JWTException;
+import org.wso2.carbon.identity.application.common.model.ClaimMapping;
+import org.wso2.carbon.identity.oauth.cache.UserAttributesCache;
+import org.wso2.carbon.identity.oauth.cache.UserAttributesCacheEntry;
+import org.wso2.carbon.identity.oauth.cache.UserAttributesCacheKey;
+import org.wso2.carbon.identity.oauth.endpoint.user.UserInfoClaimRetriever;
+import org.wso2.carbon.identity.oauth.endpoint.user.UserInfoEndpointException;
+import org.wso2.carbon.identity.oauth.endpoint.user.UserInfoResponseBuilder;
+import org.wso2.carbon.identity.oauth2.dto.OAuth2TokenValidationResponseDTO;
+
+import java.util.Map;
+
+public class UserInfoJWTResponse implements UserInfoResponseBuilder {
+
+	public String getResponseString(OAuth2TokenValidationResponseDTO tokenResponse)
+                                                                                   throws UserInfoEndpointException, OAuthSystemException {
+        Map<ClaimMapping, String> userAttributes = getUserAttributesFromCache(tokenResponse);
+        UserInfoClaimRetriever retriever = UserInfoEndpointConfig.getInstance().getUserInfoClaimRetriever();
+		Map<String, Object> claims = retriever.getClaimsMap(userAttributes);
+		
+		JWTBuilder jwtBuilder = new JWTBuilder();
+		try {
+			return jwtBuilder.setClaims(claims).buildJWT();
+		} catch (JWTException e) {
+			throw new UserInfoEndpointException("Error while generating the response JWT");
+		}
+    }
+
+    private Map<ClaimMapping, String> getUserAttributesFromCache(OAuth2TokenValidationResponseDTO tokenResponse) {
+        UserAttributesCacheKey cacheKey = new UserAttributesCacheKey(tokenResponse.getAuthorizationContextToken().getTokenString());
+        UserAttributesCacheEntry cacheEntry = (UserAttributesCacheEntry) UserAttributesCache.getInstance().getValueFromCache(cacheKey);
+        return cacheEntry.getUserAttributes();
+    }
+
+}
