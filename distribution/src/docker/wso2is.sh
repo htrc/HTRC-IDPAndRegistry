af7ef9578189ecaedeebab339cf88f63719ac33d
Milinda Pathirage <milinda.pathirage@gmail.com>
2015-05-22 14:08:42 -0400

    add a script to run idpandregistry as a daemon

--- distribution/src/docker/wso2is.sh
+++ distribution/src/docker/wso2is.sh
@@ -2,8 +2,5 @@
 #!/bin/sh
 # `/sbin/setuser wso2is` runs the given command as the user `wso2is`.
 # If you omit that part, the command will be run as root.
-exec /sbin/setuser wso2is /usr/bin/memcached 
-
-### In Dockerfile:
-RUN mkdir /etc/service/wso2is
-ADD wso2is.sh /etc/service/wso2is/run
+exec /sbin/setuser wso2is /etc/profile.d/java-env.sh
+exec /sbin/setuser wso2is /opt/htrc-idpandregistry-1.1.0-SNAPSHOT/bin/wso2server.sh start 
