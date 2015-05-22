2db7f9e047d62d6b4127eb381b42bdcf588dfd88
Milinda Pathirage <milinda.pathirage@gmail.com>
2015-05-22 14:57:53 -0400

    moving back to run idpandregistry as root user

--- distribution/src/docker/wso2is.sh
+++ distribution/src/docker/wso2is.sh
@@ -2,5 +2,4 @@
 #!/bin/sh
 # `/sbin/setuser wso2is` runs the given command as the user `wso2is`.
 # If you omit that part, the command will be run as root.
-exec /sbin/setuser wso2is /etc/profile.d/java-env.sh
-exec /sbin/setuser wso2is /opt/htrc-idpandregistry-1.1.0-SNAPSHOT/bin/wso2server.sh start 
+exec /opt/htrc-idpandregistry-1.1.0-SNAPSHOT/bin/wso2server.sh start > /dev/null &
