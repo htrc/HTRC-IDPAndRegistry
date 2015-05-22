99e62bf449841da89f8bd8b2a3d5d61a6e27339f
Milinda Pathirage <milinda.pathirage@gmail.com>
2015-05-22 15:02:28 -0400

    debug call to figureout why './run: exec format error' is given

--- distribution/src/docker/wso2is.sh
+++ distribution/src/docker/wso2is.sh
@@ -1,5 +1,2 @@
-### In wso2is.sh (make sure this file is chmod +x):
 #!/bin/sh
-# `/sbin/setuser wso2is` runs the given command as the user `wso2is`.
-# If you omit that part, the command will be run as root.
 exec /opt/htrc-idpandregistry-1.1.0-SNAPSHOT/bin/wso2server.sh start > /dev/null &
