2f294d3b22ddf3b85972cebd32f72eeb2a2beedc
Milinda Pathirage <milinda.pathirage@gmail.com>
2016-01-26 18:22:35 -0500

    updates to IDP docker image including addition of dumb-init

--- distribution/src/docker/wso2is.sh
+++ distribution/src/docker/wso2is.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-exec /opt/htrc-idpandregistry-1.1.0-SNAPSHOT/bin/wso2server.sh start > /dev/null &
+
+if [ -d "$IDP_CONF_DIR" ]; then
+  rm -r $IDP_ROOT/repository/conf
+  ln -s $IDP_CONF_DIR $IDP_ROOT/repository/conf
+fi
+
+exec $IDP_ROOT/bin/wso2server.sh
