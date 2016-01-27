9a285c4962f8d9ab86a50d11c150467916028750
Milinda Pathirage <milinda.pathirage@gmail.com>
2016-01-26 20:28:37 -0500

    added note on how to handle custom IDP configurations.

--- distribution/src/docker/wso2is.sh
+++ distribution/src/docker/wso2is.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# If IDP configuration is available through a Docker volume, remove the default configuration
+# and replace it with the configuration supplied by the volume.
 if [ -d "$IDP_CONF_DIR" ]; then
   rm -r $IDP_ROOT/repository/conf
   ln -s $IDP_CONF_DIR $IDP_ROOT/repository/conf
